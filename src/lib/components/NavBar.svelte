6ce7166f3aa75fca182ca7bc66604448f601c3e3
Derek Mason <3derekmason@gmail.com>
2022-08-27 09:13:45 -0600

    tilt init

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -3,25 +3,24 @@ import CodeBraces from "svelte-material-icons/CodeBraces.svelte";
 import Home from "svelte-material-icons/Home.svelte";
 import HumanGreeting from "svelte-material-icons/HumanGreeting.svelte";
 import TextBox from "svelte-material-icons/TextBox.svelte";
-
 </script>
 
-<div class="nav-container">
+<div id="nav-container">
   <Home size='3em'/>
   <CodeBraces size='3em' />
   <TextBox size='3em' />
   <HumanGreeting size='3em' />
-
 </div>
 
 <style>
-  .nav-container {
+  #nav-container {
     width: 240px;
     height: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
+    gap: 24px;
     box-shadow: 0 0 2px var(--secondary-light);
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import './app.css'
-import App from './App.svelte'
+import "./app.css";
+import VanillaTilt from "vanilla-tilt";
+import App from "./App.svelte";
 
 const app = new App({
-  target: document.getElementById('app')
-})
+  target: document.getElementById("app"),
+});
+const navbar = document.getElementById("nav-container");
+VanillaTilt.init(navbar, {
+  max: 25,
+  speed: 400,
+});
 
-export default app
+export default app;
