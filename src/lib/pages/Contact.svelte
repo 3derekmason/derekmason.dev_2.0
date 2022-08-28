36bc8c2b8d6591bcfc005c988ec6e6f4ec659dd3
Derek Mason <3derekmason@gmail.com>
2022-08-28 09:30:31 -0600

    oostcard form and styling

--- src/lib/pages/Contact.svelte
+++ src/lib/pages/Contact.svelte
@@ -1,19 +1,138 @@
 <script>
+import ArrowRightThick from "svelte-material-icons/ArrowRightThick.svelte";
 
 </script>
 
 <div class="contact">
-<h1>Contact me.</h1>
+  <h2>Get in touch!</h2>
+  <div id="postcard">
+    <div class="from">
+      <p>Dear Derek,</p>
+      <textarea rows="12" placeholder="Your message/comment here..."></textarea>
+      <p>From,</p>
+      <input type="text" placeholder="Your name here">
+    </div>
+    <div class="to">
+      <div class="top">
+        <div class="stamp"></div>
+      </div>
+      <div class="row">Derek Mason</div>
+      <div class="row">Colorado, USA</div>
+      <div class="row">3derekmason@gmail.com</div>
+    </div>
+  </div>
+  <button>Send <ArrowRightThick size="2em"/></button>
 </div>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Handlee&display=swap');
   .contact{
-    height: 100%;
+  height: 100%;
   width: 100%;
-  position: relative;
-  scroll-snap-align: start;
-  scroll-snap-stop: always;
-  display: grid;
-  place-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 24px;
+  }
+
+  h2 {
+    font-family: var(--font-code);
+    font-size: 32px;
+    letter-spacing: 6px;
+    background: radial-gradient(ellipse at right,
+    var(--text-color),
+    var(--secondary-light)
+  );
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  }
+
+  p {
+    color: var(--secondary-light-dull)
+  }
+  input {
+    height: 32px;
+    background: none;
+    border: none;
+    font-family: var(--font-code);
+  }
+
+  textarea {
+    background: none;
+    border: none;
+    resize: none;
+    font-family: var(--font-code);
+  }
+
+  input:focus, textarea:focus {
+    outline: none;
+  }
+
+  #postcard {
+    background: #fefefe20;
+    border-radius: 4px;
+    width: 50%;
+    height: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px;
+  }
+
+  .to, .from {
+    padding: 4px;
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .to {
+    border-left: 0.5px solid #fefefe80;
+  }
+
+  .top {
+    height: 40%;
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .stamp {
+    height: 64px;
+    width: 64px;
+    border: 1px solid var(--secondary-light-dull);
+  }
+
+  .row {
+    font-family: 'Handlee', cursive;
+    letter-spacing: 2px;
+    padding-left: 4px;
+    margin-top: 16px;
+    border-bottom: 0.5px solid var(--secondary-light-dull);
+  }
+
+  button {
+    font-family: var(--font-code);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 16px;
+    text-transform: uppercase;
+    font-size: 1.25em;
+    font-weight: 600;
+    letter-spacing: 4px;
+    border-radius: 4px;
+    width: 160px;
+    height: 40px;
+    background: var(--secondary-main);
+    box-shadow: 0 4px 0 var(--secondary-dark);
+  }
+  button:hover{
+    color: var(--text-color)
+  }
+  button:active {
+    box-shadow: none;
+    transform: translateY(4px);
   }
 </style>
