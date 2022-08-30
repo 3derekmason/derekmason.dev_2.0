aeba4d47d065c667eb4bb94b1019324ee4e7da60
Derek Mason <3derekmason@gmail.com>
2022-08-30 15:57:19 -0600

    format fetched project data

--- src/lib/pages/Projects.svelte
+++ src/lib/pages/Projects.svelte
@@ -1,9 +1,27 @@
 <script>
-
+import { onMount } from 'svelte';
+let projectData = []
+const fetchProjectData = () => {
+  fetch('https://dev-mail.vercel.app/api/projects').then((res) => res.json()).then((data) => {
+   const datajson = JSON.parse(data.message);
+   datajson.forEach((item) => {
+    item.imgs = item.imgs.split('+/+')
+   })
+   projectData = datajson;
+  })
+}
+onMount(() => {
+  fetchProjectData()
+})
 </script>
 
 <div id="projects">
-<h1>Here are my projects</h1>
+  <h2>Recent Projects:</h2>
+  <div class="container">
+    {#each projectData as project}
+    <div><h3>{project.title}</h3></div>
+    {/each}
+  </div>
 </div>
 
 <style>
@@ -13,8 +31,20 @@
   position: relative;
   scroll-snap-align: start;
   scroll-snap-stop: always;
-  display: grid;
-  place-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   background: var(--bg-color);
+  }
+  .container {
+    width: 80%;
+    height: 80%;
+    border: 1px solid var(--highlight-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 16px;
   }
 </style>
